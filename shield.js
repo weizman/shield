48f7638e5634cf94aaa3047b6cebee9a4ba69e82
weizman <weizmangal@gmail.com>
2024-08-26 19:04:33 +0300

    support in indexed props and improve stuff

--- shield.js
+++ shield.js
@@ -2,14 +2,26 @@
     const blocked = [];
     const allowlist = (document.currentScript.getAttribute('allowlist') || '').split(',');
 
-    const legitDocumentDomProps = ['documentElement', 'body', 'head', 'scrollingElement', 'firstElementChild', 'lastElementChild', 'activeElement', 'firstChild', 'lastChild'];
+    const specialDocumentGetters = {
+        documentElement: Object.getOwnPropertyDescriptor(Document.prototype, 'documentElement').get,
+        body: Object.getOwnPropertyDescriptor(Document.prototype, 'body').get,
+        head: Object.getOwnPropertyDescriptor(Document.prototype, 'head').get,
+        scrollingElement: Object.getOwnPropertyDescriptor(Document.prototype, 'scrollingElement').get,
+        firstElementChild: Object.getOwnPropertyDescriptor(Document.prototype, 'firstElementChild').get,
+        lastElementChild: Object.getOwnPropertyDescriptor(Document.prototype, 'lastElementChild').get,
+        activeElement: Object.getOwnPropertyDescriptor(Document.prototype, 'activeElement').get,
+        firstChild: Object.getOwnPropertyDescriptor(Node.prototype, 'firstChild').get,
+        lastChild: Object.getOwnPropertyDescriptor(Node.prototype, 'lastChild').get,
+    };
 
     observe(document.documentElement);
 
     function block(object, value, name, node) {
-        blocked.push(JSON.stringify({object, value}));
         const tracked = [];
         while (node) {
+            if (specialDocumentGetters[value]?.call(document) === node) {
+                break;
+            }
             if (!(node instanceof HTMLCollection || node instanceof Element)) {
                 break;
             }
@@ -23,10 +35,14 @@
             }
             node = window[object][value];
         }
+        if (!isNaN(parseInt(value))) {
+            return;
+        }
         Object.defineProperty(window[object], value, {get: function() {
                 throw new Error(`${object}["${value}"] access attempt was intercepted:`);
             }
         });
+        blocked.push(JSON.stringify({object, value}));
         for (const node of tracked) {
             for (const prop of ['id', 'name']) {
                 if (node.hasAttribute('shield_' + prop)) {
@@ -47,9 +63,6 @@
             return;
         }
         if (name !== 'id' && name !== 'name') {
-            return;
-        }
-        if (legitDocumentDomProps.includes(value)) {
             return;
         }
         if (document[value] instanceof Element) {
--- test.js
+++ test.js
@@ -1,3 +1,65 @@
+function assert(test) {
+    function format(results) {
+        // [ [null] , [null] ]
+        if (results[0][0] === null && results[1][0] === null) {
+            return [false, test, `UNEXPECTED TEST RESULT (TESTER MUST CHECK THIS)`];
+        }
+        // [ [null] , [false] ]
+        // [ [null] , [true] ]
+        if (results[0][0] === null) {
+            return results[1];
+        }
+        // [ [true] , [null] ]
+        // [ [false] , [null] ]
+        if (results[1][0] === null) {
+            return results[0];
+        }
+        // [ [false] , [false] ]
+        // [ [false] , [true] ]
+        if (results[0][0] === false) {
+            return results[0];
+        }
+        // [ [false] , [false] ]
+        // [ [true] , [false] ]
+        if (results[1][0] === false) {
+            return results[1];
+        }
+        // [ [true] , [true] ]
+        if (results[0][0] === true && results[1][0] === true) {
+            return results[1];
+        }
+        // [ ??? ]
+        throw new Error(`UNEXPECTED TEST RESULT (TESTER MUST CHECK THIS)`);
+    }
+    return format(['window', 'document'].map(object => {
+        try {
+            if (window[object][test] instanceof Element) {
+                return [false, `shield failed to prevent clobbering of ${object}["${test}"] (Element)`];
+            }
+            if (window[object][test] instanceof HTMLCollection) {
+                return [false, `shield failed to prevent clobbering of ${object}["${test}"] (HTMLCollection)`];
+            }
+            for (let i = 0; i < Infinity; i++) {
+                if (!window[i]) {
+                    break;
+                }
+                if (window[object][test] === window[i] && window[i] === window[i].window) {
+                    return [false, `shield failed to prevent clobbering of ${object}["${test}"] (Window)`];
+                }
+            }
+            if (!isNaN(test)) {
+                return [true, `shield worked, environment removed numeric props ${object}["${test}"] (undefined)`];
+            }
+        } catch (err) {
+            if (err.message === `${object}["${test}"] access attempt was intercepted:`) {
+                return [true, `shield worked, environment blocked clobber attempt ${object}["${test}"] (Error)`];
+            }
+            return [false, `UNEXPECTED TEST RESULT WITH ERROR (TESTER MUST CHECK THIS): "${err.message}"`]
+        }
+        return [null];
+    }));
+}
+
 window.TEST = (function(){
     return async function main(container, object) {
         function setup() {
@@ -15,52 +77,6 @@ window.TEST = (function(){
             return [
                 document.getElementById('tests-' + object).appendChild(document.createElement('div')),
             ];
-        }
-
-        function assert(test) {
-            const results = ['window', 'document'].map(object => {
-                try {
-                    if (window[object][test] instanceof Element) {
-                        return [false, test, `shield failed to prevent clobbering of ${object}["${test}"] (Element)`];
-                    }
-                    if (window[object][test] instanceof HTMLCollection) {
-                        return [false, test, `shield failed to prevent clobbering of ${object}["${test}"] (HTMLCollection)`];
-                    }
-                    for (let i = 0; i < Infinity; i++) {
-                        if (!window[i]) {
-                            break;
-                        }
-                        if (window[object][test] === window[i] && window[i] === window[i].window) {
-                            return [false, test, `shield failed to prevent clobbering of ${object}["${test}"] (Window)`];
-                        }
-                    }
-                } catch (err) {
-                    if (err.message === `${object}["${test}"] access attempt was intercepted:`) {
-                        return [true, test, `shield worked, environment did not clobber ${object}["${test}"] (undefined)`];
-                    }
-                    return [false, test, `UNEXPECTED TEST RESULT WITH ERROR (TESTER MUST CHECK THIS): "${err.message}"`]
-                }
-                return [null];
-            });
-            if (results[0][0] === null && results[1][0] === null) {
-                return [false, test, `UNEXPECTED TEST RESULT (TESTER MUST CHECK THIS)`];
-            }
-            if (results[0][0] === null) {
-                return results[1];
-            }
-            if (results[1][0] === null) {
-                return results[0];
-            }
-            if (results[0][0] === false) {
-                return results[0];
-            }
-            if (results[1][0] === false) {
-                return results[1];
-            }
-            if (results[0][0] === true && results[1][0] === true) {
-                return results[1];
-            }
-            throw new Error(`UNEXPECTED TEST RESULT (TESTER MUST CHECK THIS)`);
         }
 
         const tests = [
@@ -81,7 +97,7 @@ window.TEST = (function(){
                 const child = div.appendChild(document.createElement('span'));
                 child.id = test;
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             async function(div) {
@@ -89,7 +105,7 @@ window.TEST = (function(){
                 const child = div.appendChild(document.createElement('span'));
                 child.setAttribute('id', test);
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             async function(div) {
@@ -98,7 +114,7 @@ window.TEST = (function(){
                 child.id = test;
                 div.appendChild(child);
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             async function(div) {
@@ -107,7 +123,7 @@ window.TEST = (function(){
                 child.setAttribute('id', test);
                 div.appendChild(child);
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             // name
@@ -118,7 +134,7 @@ window.TEST = (function(){
                 child.setAttribute('name', test);
                 div.appendChild(child);
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             async function(div) {
@@ -126,7 +142,7 @@ window.TEST = (function(){
                 const child = div.appendChild(document.createElement('iframe'));
                 child.setAttribute('name', test);
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             async function(div) {
@@ -135,7 +151,7 @@ window.TEST = (function(){
                 child.setAttribute('name', test);
                 div.appendChild(child);
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             async function(div) {
@@ -144,7 +160,7 @@ window.TEST = (function(){
                 child.setAttribute('name', test);
                 div.appendChild(child);
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             async function(div) {
@@ -156,7 +172,7 @@ window.TEST = (function(){
                 div.appendChild(child);
                 div.appendChild(child2);
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             async function(div) {
@@ -168,42 +184,113 @@ window.TEST = (function(){
                 child.appendChild(child2);
                 div.appendChild(child);
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             async function(div) {
                 const test = object + (' : non html element (svg)');
                 div.innerHTML = `<svg id="${test}">`;
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             async function(div) {
                 const test = object + (' : non html element (math)');
                 div.innerHTML = `<math id="${test}">`;
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             async function(div) {
                 const test = 'querySelector';
-                div.innerHTML = `<embed name="${test}">`;
+                div.innerHTML = `<embed name="${test}" id="${test}">`;
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
 
             async function(div) {
                 const test = 'cookie';
-                div.innerHTML = `<embed name="${test}">`;
+                div.innerHTML = `<embed name="${test}" id="${test}">`;
                 await new Promise(r => setTimeout(r, 0));
-                return assert(test);
+                return [test, ...assert(test)];
             },
+
+            async function(div) {
+                const test = 'documentElement';
+                div.innerHTML = `<embed name="${test}" id="${test}">`;
+                await new Promise(r => setTimeout(r, 0));
+                return [test, ...assert(test)];
+            },
+
+            async function(div) {
+                const test = 'body';
+                div.innerHTML = `<embed name="${test}" id="${test}">`;
+                await new Promise(r => setTimeout(r, 0));
+                return [test, ...assert(test)];
+            },
+
+            async function(div) {
+                const test = 'head';
+                div.innerHTML = `<embed name="${test}" id="${test}">`;
+                await new Promise(r => setTimeout(r, 0));
+                return [test, ...assert(test)];
+            },
+
+            async function(div) {
+                const test = 'scrollingElement';
+                div.innerHTML = `<embed name="${test}" id="${test}">`;
+                await new Promise(r => setTimeout(r, 0));
+                return [test, ...assert(test)];
+            },
+
+            async function(div) {
+                const test = 'firstElementChild';
+                div.innerHTML = `<embed name="${test}" id="${test}">`;
+                await new Promise(r => setTimeout(r, 0));
+                return [test, ...assert(test)];
+            },
+
+            async function(div) {
+                const test = 'lastElementChild';
+                div.innerHTML = `<embed name="${test}" id="${test}">`;
+                await new Promise(r => setTimeout(r, 0));
+                return [test, ...assert(test)];
+            },
+
+            async function(div) {
+                const test = 'activeElement';
+                div.innerHTML = `<embed name="${test}" id="${test}">`;
+                await new Promise(r => setTimeout(r, 0));
+                return [test, ...assert(test)];
+            },
+
+            async function(div) {
+                const test = 'firstChild';
+                div.innerHTML = `<embed name="${test}" id="${test}">`;
+                await new Promise(r => setTimeout(r, 0));
+                return [test, ...assert(test)];
+            },
+
+            async function(div) {
+                const test = 'lastChild';
+                div.innerHTML = `<embed name="${test}" id="${test}">`;
+                await new Promise(r => setTimeout(r, 0));
+                return [test, ...assert(test)];
+            },
+
+            async function(div) {
+                const test = '666';
+                div.innerHTML = `<embed name="${test}" id="${test}">`;
+                await new Promise(r => setTimeout(r, 0));
+                return [test, ...assert(test)];
+            },
+
         ];
 
         setup();
         for (const test of tests) {
             const [div] = before();
-            const [passed, tester, reason] = await test(div);
+            const [tester, passed, reason] = await test(div);
             let msg, color;
             if (!passed) {
                 const result = document.getElementById('result-' + object);
